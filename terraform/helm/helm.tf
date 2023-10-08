dcf1d9d81231beb66b93c05a381e141fe300f952
John Dodson <john.dodson@voice.com>
2023-10-08 10:10:09 -0400

    split up apply steps

--- terraform/helm/helm.tf
+++ terraform/helm/helm.tf
@@ -4,6 +4,35 @@ variable "chart_root" {
   default     = "../helm"
 }
 
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "EsserLabs"
+    workspaces = {
+      name = "rearc-quest"
+    }
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 resource "helm_release" "rearc_quest" {
   name       = "rearc-quest"
   chart      = "${var.chart_root}/chart"
--- terraform/main.tf
+++ terraform/main.tf
@@ -17,27 +17,6 @@ module "eks" {
   source = "./eks"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    command     = "aws"
-  }
-}
-
 module "helm" {
   source = "./helm"
-  depends_on = [module.eks]
 }
