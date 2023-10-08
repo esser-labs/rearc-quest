cd371875491bdd874633a66c16c9791957020a74
John Dodson <john.dodson@voice.com>
2023-10-08 10:01:02 -0400

    correct module path on eks cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,12 +18,12 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.module.eks.cluster_name
   depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.module.eks.cluster_name
   depends_on = [module.eks]
 }
 
