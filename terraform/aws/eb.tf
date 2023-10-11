a518cd6007da5b35b7bd3ffe38f7097f6419c70f
John Dodson <john.dodson@voice.com>
2023-10-11 12:24:24 -0400

    Add secret word

--- terraform/aws/eb.tf
+++ terraform/aws/eb.tf
@@ -82,12 +82,6 @@ services:
 EOF
 }
 
-resource "aws_s3_object" "app_zip" {
-  bucket = aws_s3_bucket.jed_rearc_quest.id
-  key    = "${var.last_run_commit}.zip"
-  source = "${path.module}/${var.last_run_commit}.zip"
-}
-
 resource "aws_elastic_beanstalk_application" "jed_rearc_quest" {
   name        = "${var.elastic_name}"
   description = "${var.elastic_name}"
@@ -112,22 +106,33 @@ resource "aws_elastic_beanstalk_environment" "jed_rearc_quest" {
     value     = "True"
   }
 
+  setting { 
+    namespace = "aws:elbv2:listener:default"
+    name      = "ListenerEnabled"
+    value     = false
+    }
+    
   setting {
-    namespace = "aws:elbv2:listener:443"
-    name = "ListenerEnabled"
-    value = aws_acm_certificate.cert.arn == "" ? "false" : "true"
+      namespace = "aws:elbv2:listener:443"
+      name      = "ListenerEnabled"
+      value     = true
   }
 
   setting {
-    namespace = "aws:elbv2:listener:443"
-    name = "Protocol"
-    value = "HTTPS"
-  }
+      namespace = "aws:elbv2:listener:443"
+      name      = "Protocol"
+      value     = "HTTPS"
+   }
 
   setting {
     namespace = "aws:elbv2:listener:443"
     name = "SSLCertificateArns"
     value = aws_acm_certificate.cert.arn
+  }
+  setting {
+      namespace = "aws:elbv2:listener:443"
+      name      = "SSLPolicy"
+      value     = "ELBSecurityPolicy-2016-08"
   }
 }
 
@@ -137,43 +142,4 @@ resource "aws_elastic_beanstalk_application_version" "jed_rearc_quest" {
   description = "application version created by terraform"
   bucket      = aws_s3_bucket.jed_rearc_quest.id
   key         = aws_s3_object.docker_compose.id
-}
-
-resource "aws_elastic_beanstalk_application" "jed_rearc_quest_node" {
-  name        = "${var.elastic_name}-node"
-  description = "${var.elastic_name}-node"
-}
-
-resource "aws_elastic_beanstalk_environment" "jed_rearc_quest_node" {
-  name         = "${var.elastic_name}-node"
-  application  = aws_elastic_beanstalk_application.jed_rearc_quest_node.name
-  cname_prefix = "rearc-quest-node"
-
-  solution_stack_name = "64bit Amazon Linux 2023 v6.0.1 running Node.js 18" 
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.beanstalk_iam_instance_profile.arn
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
-    name      = "StreamLogs"
-    value     = "True"
-  }
-  
-  setting {
-      namespace = "aws:elasticbeanstalk:application:environment" 
-      name = "NODE_ENV" 
-      value = "production" 
-  }
-}
-
-resource "aws_elastic_beanstalk_application_version" "jed_rearc_quest_node" {
-  name        = "${var.elastic_name}-${var.last_run_commit}"
-  application = aws_elastic_beanstalk_application.jed_rearc_quest_node.name
-  description = "application version created by terraform"
-  bucket      = aws_s3_bucket.jed_rearc_quest.id
-  key         = aws_s3_object.app_zip.id
 }
